372702772145c555be48edfd384ef89207ec13a5
clopez <clopez@aszende.com>
2022-04-15 18:50:00 +0200

    Test  button

--- src/screens/Restaurants/RestaurantsScreen.js
+++ src/screens/Restaurants/RestaurantsScreen.js
@@ -1,10 +1,25 @@
+// Dependencias
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+// Configs
+import { StacksConfig } from '../../utils';
 
-export function RestaurantsScreen(){
+export function RestaurantsScreen(props){
+
+    const { navigation } = props;
+
+    console.log(navigation);
+
+    const goToAddRestaurant = () => {
+        // Primero le pasamos el tab del stack, luego le decimos qué screen (tab) queremos que se muestre de dentro de ese stack
+        navigation.navigate(StacksConfig.restaurant.tab, { screen: StacksConfig.restaurant.screens.addRestaurant.tab });
+    }
+
     return(
         <View>
             <Text>RestaurantsScreen</Text>
+            <Button title={'Crear Restaurante'} onPress={goToAddRestaurant}/>
         </View>
     );
 }

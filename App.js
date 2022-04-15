0b80384f58803185a1c9d073d6657163e018de40
clopez <clopez@aszende.com>
2022-04-15 19:07:16 +0200

    Firebase added

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigation } from './src/navigation/AppNavigation';
+import { initFirebase } from './src/utils';
 
 export default function App() {
+
   return (
     <>
       <NavigationContainer>

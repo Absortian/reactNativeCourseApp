82a45c49ac71fb0000a83641e7e5f5d3c02bbb79
absor <absortian@gmail.com>
2022-04-21 20:13:16 +0200

    Register form validation

--- src/components/Auth/RegisterForm/RegisterForm.data.js
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -1,7 +1,23 @@
+import * as Yup from "yup";
+
 export function initialValues(){
     return {
         email: "",
         password: "",
         passwordConfirmation: ""
     };
+}
+
+export function validationSchema(){
+    return Yup.object().shape({
+        email: Yup.string()
+            .email("El correo electrónico no es válido")
+            .required("El correo electrónico es obligatorio"),
+        password: Yup.string()
+            .min(6, "La contraseña debe tener al menos 6 caracteres")
+            .required("La contraseña es obligatoria"),
+        passwordConfirmation: Yup.string()
+            .oneOf([Yup.ref("password")], "Las contraseñas no coinciden")
+            .required("La confirmación de la contraseña es obligatoria")
+    });
 }
--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -3,12 +3,13 @@ import { View } from 'react-native';
 import { Button, Icon, Input } from 'react-native-elements';
 import { Styles } from './RegisterForm.styles';
 import { useFormik } from "formik";
-import { initialValues } from './RegisterForm.data';
+import { initialValues, validationSchema } from './RegisterForm.data';
 
 export function RegisterForm(){
 
     const formik = useFormik({
         initialValues: initialValues(),
+        validationSchema: validationSchema(),
         onSubmit: formValue => {
             console.log(formValue);
         }
@@ -16,9 +17,9 @@ export function RegisterForm(){
 
     return(
         <View style={Styles.content}>
-            <Input onChangeText={text => formik.setFieldValue("email",text)} placeholder='Correo electrónico' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'envelope-o' }} />
-            <Input onChangeText={text => formik.setFieldValue("password",text)} placeholder='Contraseña' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'key' }} secureTextEntry />
-            <Input onChangeText={text => formik.setFieldValue("passwordConfirmation",text)} placeholder='Confirmar contraseña' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'key' }} secureTextEntry />
+            <Input errorMessage={formik.errors.email} onChangeText={text => formik.setFieldValue("email",text)} placeholder='Correo electrónico' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'envelope-o' }} />
+            <Input errorMessage={formik.errors.password} onChangeText={text => formik.setFieldValue("password",text)} placeholder='Contraseña' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'key' }} secureTextEntry />
+            <Input errorMessage={formik.errors.passwordConfirmation} onChangeText={text => formik.setFieldValue("passwordConfirmation",text)} placeholder='Confirmar contraseña' containerStyle={Styles.input} leftIcon={{ type: 'font-awesome', name: 'key' }} secureTextEntry />
             <Button title='Registrarse' containerStyle={Styles.btnContainer} buttonStyle={Styles.btn} onPress={() => formik.handleSubmit()}/>
         </View>
     );
